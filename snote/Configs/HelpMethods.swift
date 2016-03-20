c53d90bf6627caa841446f84fa79c6ad58d8a80b
nevercry <ysfnevercry@gmail.com>
2016-03-21 03:37:13 +0800

    Update Directs Hierarchical

--- snote/Configs/HelpMethods.swift
+++ snote/Configs/HelpMethods.swift
@@ -23,17 +23,17 @@ extension UIButton {
 }
 
 extension UIViewController {
-//    // 验证有效手机号
-//    func isValidMobile(mobile: String) -> Bool {
-//        let mobiePhoneRex = "^0?(13[0-9]|15[012356789]|17[0678]|18[0-9]|14[57])[0-9]{8}$"
-//        return RegexHelper.init(mobiePhoneRex).match(mobile)
-//    }
-//    
-//    // 验证有效用户名 6-20位 英文和数字组合
-//    func isValidUserName(userName: String) -> Bool {
-//        let userNameRex = "^[A-Za-z0-9]{6,20}+$"
-//        return RegexHelper.init(userNameRex).match(userName)
-//    }
+    // 验证有效手机号
+    func isValidMobile(mobile: String) -> Bool {
+        let mobiePhoneRex = "^0?(13[0-9]|15[012356789]|17[0678]|18[0-9]|14[57])[0-9]{8}$"
+        return RegexHelper.init(mobiePhoneRex).match(mobile)
+    }
+    
+    // 验证有效用户名 6-20位 英文和数字组合
+    func isValidUserName(userName: String) -> Bool {
+        let userNameRex = "^[A-Za-z0-9]{6,20}+$"
+        return RegexHelper.init(userNameRex).match(userName)
+    }
     
     func alertViewShow(title:String, andMessage message: String) {
         alerViewShow(title, andMessage: message, andHandler: nil)
---
+++ snote/Configs/snoteConfig.swift
@@ -0,0 +1,9 @@
+//
+//  snoteConfig.swift
+//  snote
+//
+//  Created by nevercry on 3/21/16.
+//  Copyright © 2016 nevercry. All rights reserved.
+//
+
+import Foundation
