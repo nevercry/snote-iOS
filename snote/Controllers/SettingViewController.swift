986edcde4b992646a2f8a4c622273af85d6b542e
nevercry <ysfnevercry@gmail.com>
2016-04-06 16:35:29 +0800

    Add Logout Clear Realm

--- snote/Controllers/SettingViewController.swift
+++ snote/Controllers/SettingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class SettingViewController: UIViewController {
     @IBOutlet weak var logoutButton: UIButton!
@@ -14,6 +15,26 @@ class SettingViewController: UIViewController {
     // 登出账户
     @IBAction func logout(sender: UIButton) {
         SnoteUserDefaults.clearToken()
+        
+        // 清除数据
+        let manager = NSFileManager.defaultManager()
+        let realmPath = Realm.Configuration.defaultConfiguration.path! as NSString
+        let realmPaths = [
+            realmPath as String,
+            realmPath.stringByAppendingPathExtension("lock")!,
+            realmPath.stringByAppendingPathExtension("log_a")!,
+            realmPath.stringByAppendingPathExtension("log_b")!,
+            realmPath.stringByAppendingPathExtension("note")!
+                          ]
+        for path in realmPaths {
+            do {
+                try manager.removeItemAtPath(path)
+            } catch {
+                print(error)
+                self.alertViewShow("删除数据库出错", andMessage: "请重试")
+                return
+            }
+        }
         
         let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
         appDelegate?.startSignUpAndLogin()
